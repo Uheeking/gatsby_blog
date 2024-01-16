cc57a650d8c689038aed87d7b7fe3b524227e6bc
Uheeking <wert1847@naver.com>
2024-01-16 17:53:57 +0900

    📝 ADD : grid 추가

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
+import * as React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import './layout.css'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
